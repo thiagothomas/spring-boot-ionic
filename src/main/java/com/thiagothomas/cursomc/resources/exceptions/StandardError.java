661e37f9d33bd686f7d46049d54ff5a10237192e
Thiago Thomas <thiagohthomas@gmail.com>
2020-11-17 22:20:31 -0300

    Ajuste no endpoint /categorias/{id}

---
+++ src/main/java/com/thiagothomas/cursomc/resources/exceptions/StandardError.java
@@ -0,0 +1,44 @@
+package com.thiagothomas.cursomc.resources.exceptions;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer status;
+	private String msg;
+	private Long time;
+	
+	public StandardError(Integer status, String msg, Long time) {
+		super();
+		this.status = status;
+		this.msg = msg;
+		this.time = time;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Long getTime() {
+		return time;
+	}
+
+	public void setTime(Long time) {
+		this.time = time;
+	}
+	
+	
+}
