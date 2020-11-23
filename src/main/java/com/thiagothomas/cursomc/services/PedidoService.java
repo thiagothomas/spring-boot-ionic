9b5a6df0c4ac0e2444c38ff15cdce06567904440
Thiago Thomas <thiagohthomas@gmail.com>
2020-11-23 17:06:12 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/thiagothomas/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.thiagothomas.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagothomas.cursomc.domain.Pedido;
+import com.thiagothomas.cursomc.repositories.PedidoRepository;
+import com.thiagothomas.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
