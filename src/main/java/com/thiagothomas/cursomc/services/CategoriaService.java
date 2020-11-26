8be1df86cda6b743d717ec9db32fa3d73a5938f6
Thiago Thomas <thiagohthomas@gmail.com>
2020-11-26 14:51:25 -0300

    Listando todas categorias com DTO

--- src/main/java/com/thiagothomas/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagothomas/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.thiagothomas.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir una categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
