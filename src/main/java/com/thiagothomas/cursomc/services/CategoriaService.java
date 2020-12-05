53dd309f3d8116ae8712700d66745cce20841c50
Thiago Thomas <thiagohthomas@gmail.com>
2020-12-05 15:26:10 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/thiagothomas/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagothomas/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.thiagothomas.cursomc.domain.Categoria;
+import com.thiagothomas.cursomc.dto.CategoriaDTO;
 import com.thiagothomas.cursomc.repositories.CategoriaRepository;
 import com.thiagothomas.cursomc.services.exceptions.DataIntegrityException;
 import com.thiagothomas.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linePerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
